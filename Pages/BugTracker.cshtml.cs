ecb0c56c6f813a5b411d07ec2ec5748f996be760
Abhilash Panicker <abhilash.panicker@outlook.com>
2022-11-28 20:15:45 +0530

    adding bugtracker page

---
+++ Pages/BugTracker.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BugTracker.Pages
+{
+    public class BugTrackerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
