8e0641a0b4219b704bc2d1efdf438e6fb8f77e99
Abhilash Panicker <abhilash.panicker@outlook.com>
2022-11-29 13:12:06 +0530

    pushing procmon

--- Program.cs
+++ Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.FileProviders;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,6 +18,12 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(
+           Path.Combine(builder.Environment.ContentRootPath, "Pages")),
+    RequestPath = new PathString ("/Pages")
+});
 app.UseRouting();
 
 app.UseAuthorization();
