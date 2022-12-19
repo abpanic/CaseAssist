c219e958fe933e7d464a568da853bf604df1c7d8
Abhilash Panicker <abhilash.panicker@outlook.com>
2022-12-19 22:40:39 +0530

    fixed error due to id

--- Services/CaseTransfer.cs
+++ Services/CaseTransfer.cs
@@ -7,30 +7,30 @@ public static class TransferService
 {
 
     static List<SR> SRs {get;}
-    static int nextnumber = 3;
+    static int nextId = 3;
 
     static TransferService()
     {
         SRs=new List<SR>
         {
-            new SR{number=Guid.NewGuid(), Issue="cannot login into org", IsFQRdone =true, POD =POD.PowerApps,Age = 67, IsSMEReviewed =IsSME.False},
-            new SR{number=Guid.NewGuid(), Issue="configuring CRM client for outlook", IsFQRdone =true, POD =POD.Clients, Age = 4, IsSMEReviewed =IsSME.False}
+            new SR{number=1, Issue="cannot login into org", IsFQRdone =true, POD =POD.PowerApps,Age = 67, IsSMEReviewed =IsSME.False},
+            new SR{number=2, Issue="configuring CRM client for outlook", IsFQRdone =true, POD =POD.Clients, Age = 4, IsSMEReviewed =IsSME.False}
         };
     }
 
     public static List<SR> GetAll()=> SRs;
 
-    public static SR? Get(int number) => SRs.FirstOrDefault(c =>c.number==number);
+    public static SR? Get(int id) => SRs.FirstOrDefault(c =>c.Id==id);
 
     public static void Add(SR SR)
     {
-        SR.number= nextnumber++;
+        SR.Id= nextId++;
         SRs.Add(SR);
     }
 
-    public static void Delete(int number)
+    public static void Delete(int Id)
     {
-        var SR=Get(number);
+        var SR=Get(Id);
         if (SR is null)
             return;
         
@@ -40,7 +40,7 @@ public static class TransferService
 
     public static void Update(SR SR)
     {
-        var index=SRs.FindIndex(c => c.number==SR.number);
+        var index=SRs.FindIndex(c => c.Id==SR.Id);
         if (index==-1)
          return;
         
